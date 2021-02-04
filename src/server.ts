c6dbb7f88b0b99e4c5ef2cafe8233e39b6af12f3
orla11 <orlandif.mail@gmail.com>
2021-02-05 00:37:15 +0100

    server development

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,22 @@
 import express from 'express';
 import config from './config';
+import logger from './loaders/logger';
+
+async function startServer() {
+    const NAMESPACE = 'Server';
+
+    const server = express();
+
+    await require('./loaders').default({ expressApp: server });
+
+    server
+        .listen(config.server.port, () => {
+            logger.info(`[${NAMESPACE}] - Listening on port ${config.server.port}`);
+        })
+        .on('error', (err) => {
+            logger.error(`[${NAMESPACE} - ${err}]`);
+            process.exit(1);
+        });
+}
+
+startServer();
