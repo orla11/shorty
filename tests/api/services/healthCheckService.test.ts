9f032db577b516504306334bd8343224b57ced1d
orla11 <orlandif.mail@gmail.com>
2021-02-06 00:33:07 +0100

    created test for Api Health Check Service

---
+++ tests/api/services/healthCheckService.test.ts
@@ -0,0 +1,12 @@
+import apiHealthCheckService from '../../../src/services/apiHealthCheck';
+
+describe('Api Health Check staus test', () => {
+    it('should console log apiHealthCheckService', async () => {
+        console.log = jest.fn();
+        const apiHealthCheckServiceInstance = new apiHealthCheckService();
+
+        apiHealthCheckServiceInstance.logInConsole();
+
+        expect(console.log).toHaveBeenCalledWith('apiHealthCheckService');
+    });
+});
