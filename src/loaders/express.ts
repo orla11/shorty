aaa089e6e9115ed6e427deb8ac4507963fe1502a
orla11 <orlandif.mail@gmail.com>
2021-02-06 00:15:14 +0100

    moved logging middleware at the top

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import cors from 'cors';
-// import config from '../config';
+import config from '../config';
 import logger from './logger';
+import routes from '../api';
 
 export default async ({ app }: { app: express.Application }) => {
     const NAMESPACE = 'Server';
+
+    app.use((req, res, next) => {
+        logger.info(`[${NAMESPACE}] - METHOD: [${req.method}], URL: [${req.url}], IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            logger.info(`[${NAMESPACE}] - METHOD: [${req.method}], URL: [${req.url}], IP: [${req.socket.remoteAddress}], STATUS: [${res.statusCode}]`);
+        });
+
+        next();
+    });
 
     /*
      * Health Check endpoints
@@ -18,24 +29,12 @@ export default async ({ app }: { app: express.Application }) => {
 
     app.enable('trust proxy');
 
-    app.use((req, res, next) => {
-        logger.info(`[${NAMESPACE}] - METHOD: [${req.method}], URL: [${req.url}], IP: [${req.socket.remoteAddress}]`);
-
-        res.on('finish', () => {
-            logger.info(`[${NAMESPACE}] - METHOD: [${req.method}], URL: [${req.url}], IP: [${req.socket.remoteAddress}], STATUS: [${res.statusCode}]`);
-        });
-    });
-
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
     app.use(cors());
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization');
-        return res.status(200).json({});
-    });
 
-    // app.use(config.api.prefix, routes());
+    app.use(config.api.prefix, routes());
 
     app.use((req, res, next) => {
         const error = new Error('Not Found');
