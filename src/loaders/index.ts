03adff9ac5be29ee18505198749d9a3afea8ab5d
orla11 <orlandif.mail@gmail.com>
2021-02-11 23:12:25 +0100

    added comment for injecting the model into the DI container

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -5,6 +5,11 @@ import expressLoader from './express';
 export default async ({ expressApp }: { expressApp: express.Application }) => {
     const NAMESPACE = 'Loader';
 
+    // const urlModel = {
+    //     name: 'urlModel',
+    //     model: require('../models/url').default
+    // };
+
     await expressLoader({ app: expressApp });
     logger.info(`[${NAMESPACE}] - Express loaded`);
 };
