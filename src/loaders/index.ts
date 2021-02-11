7d2f4acd9367df71a940b913801c871a48ede0dd
orla11 <orlandif.mail@gmail.com>
2021-02-11 23:21:53 +0100

    added dependencyInjectorLoader which loads the mongoose models

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import logger from './logger';
+import dependencyInjectorLoader from './dependencyInjector';
 import expressLoader from './express';
 
 export default async ({ expressApp }: { expressApp: express.Application }) => {
     const NAMESPACE = 'Loader';
 
-    // const urlModel = {
-    //     name: 'urlModel',
-    //     model: require('../models/url').default
-    // };
+    const urlModel = {
+        name: 'urlModel',
+        model: require('../models/url').default
+    };
+
+    // Injecting models into container
+    dependencyInjectorLoader({
+        models: [urlModel]
+    });
 
     await expressLoader({ app: expressApp });
     logger.info(`[${NAMESPACE}] - Express loaded`);
