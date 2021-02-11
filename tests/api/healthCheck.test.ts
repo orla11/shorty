c295390057a31c5b2661ff9b2f4222cadee31da0
orla11 <orlandif.mail@gmail.com>
2021-02-11 23:30:23 +0100

    updated tests to reflect new api prefix

--- tests/api/healthCheck.test.ts
+++ tests/api/healthCheck.test.ts
@@ -1,6 +1,7 @@
 import { default as request } from 'supertest';
 import express from 'express';
 import loaders from '../../src/loaders';
+import config from '../../src/config';
 
 const app = express();
 
@@ -10,12 +11,16 @@ beforeAll(async (done) => {
 });
 
 describe('Api Health Check', () => {
-    it('should return 200 when GET /api/status', async () => {
-        const res = await request(app).get('/api/status').send();
+    it('should return 200 when GET /api/v1/status', async () => {
+        const res = await request(app)
+            .get(config.api.prefix + 'status')
+            .send();
         expect(res.status).toBe(200);
     });
-    it('should return 404 when GET /api/nonexistingapiroute', async () => {
-        const res = await request(app).get('/api/nonexistingapiroute').send();
+    it('should return 404 when GET /api/v1/nonexistingapiroute', async () => {
+        const res = await request(app)
+            .get(config.api.prefix + '/nonexistingapiroute')
+            .send();
         expect(res.status).toBe(404);
     });
 });
