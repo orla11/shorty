b9c7973e7c4d59cc21c88642a828099d4e6191bc
orla11 <orlandif.mail@gmail.com>
2021-02-12 22:56:54 +0100

    imported shorty validators

--- src/api/routes/shorty.ts
+++ src/api/routes/shorty.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import shortyController from '../../controllers/shortyController';
+import shortyValidator from '../validators/shortyValidator';
 
 const route = Router();
 
 export default (app: Router) => {
     app.use('/shorty', route);
 
-    route.post('/', shortyController.shortUrl);
+    route.post('/', shortyValidator.validateOriginalUrl, shortyController.shortUrl);
 };
