0ae4d1768f49cb7a51bf01a3b8845f610ab6a8d7
orla11 <orlandif.mail@gmail.com>
2021-02-13 00:58:06 +0100

    created shortyService test

--- tests/api/services/shortyService.test.ts
+++ tests/api/services/shortyService.test.ts
@@ -1,5 +1,14 @@
 import shortyService from '../../../src/services/shortyService';
+import express from 'express';
+import loaders from '../../../src/loaders';
+
+const app = express();
+
+beforeAll(async (done) => {
+    await loaders({ expressApp: app });
+    done();
+});
 
 describe('shortyService test', () => {
-    it('should return')
-})
+    it('should return', async () => {});
+});
