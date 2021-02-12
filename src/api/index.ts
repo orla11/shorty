fa3f8ba8e226ffb43d1dbbfa9dfe42109149fcd7
orla11 <orlandif.mail@gmail.com>
2021-02-12 14:48:17 +0100

    imported shorty route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import apiHealthCheck from './routes/apiHealthCheck';
+import shorty from './routes/shorty';
 
 export default () => {
     const app = Router();
+
     apiHealthCheck(app);
+    shorty(app);
 
     return app;
 };
