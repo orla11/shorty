256212501886e12ed09bacbf52e30d990365f587
orla11 <orlandif.mail@gmail.com>
2021-02-13 00:59:40 +0100

    added mongoose loader

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import logger from './logger';
 import dependencyInjectorLoader from './dependencyInjector';
+import mongooseLoader from './database';
 import expressLoader from './express';
 
 export default async ({ expressApp }: { expressApp: express.Application }) => {
     const NAMESPACE = 'Loader';
+
+    await mongooseLoader();
+    logger.info(`[${NAMESPACE}] - DB loaded and connected`);
 
     const urlModel = {
         name: 'urlModel',
