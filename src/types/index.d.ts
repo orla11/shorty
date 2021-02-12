2c77178c7da103e7dee405858dd0fef716d79ad4
orla11 <orlandif.mail@gmail.com>
2021-02-12 23:10:40 +0100

    moved namespaces file into express folder

--- src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Document, Model } from 'mongoose';
-import { IUrl } from '../../src/interfaces/IUrl';
-
-declare global {
-    namespace Models {
-        export type UrlModel = Model<IUrl & Document>;
-    }
-}
