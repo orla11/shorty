aadffdb8770a9fa9ebce718fd8b1462905078264
orla11 <orlandif.mail@gmail.com>
2021-02-16 23:00:17 +0100

    added try-catch block in database loaders

--- src/loaders/database.ts
+++ src/loaders/database.ts
@@ -1,12 +1,18 @@
 import mongoose from 'mongoose';
 import { Db } from 'mongodb';
 import config from '../config';
+import logger from './logger';
 
 export default async (): Promise<Db> => {
-    const connection = await mongoose.connect(config.database.URL, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    });
-    return connection.connection.db;
+    try {
+        const connection = await mongoose.connect(config.database.URL, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        });
+        return connection.connection.db;
+    } catch (error) {
+        logger.error('🔥 Error on mongoose loader: %o', error);
+        throw error;
+    }
 };
