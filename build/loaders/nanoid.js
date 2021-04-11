4bd92c5987776b2f66f170f63990df91d07d53cb
orla11 <orlandif.mail@gmail.com>
2021-04-11 15:21:59 +0200

    .gitignore fixed

--- build/loaders/nanoid.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var nanoid_1 = require("nanoid");
-var customAlphabetString = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-var nanoidInstance = nanoid_1.customAlphabet(customAlphabetString, 10);
-exports.default = nanoidInstance;
